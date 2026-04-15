83c69cafdc9740fcccf4d87cee8520c375f12386
Felipe da Silva Gonçalves <felipedsg.work@gmail.com>
2026-04-15 13:40:56 -0300

    feat: configura estrutura inicial do projeto com docker, php e mysql

---
+++ backend/config/database.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$dbname = getenv('DB_DATABASE');
+$user = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
+        $user,
+        $password
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('Erro na conexão com o banco: ' . $e->getMessage());
+}
--- public/index.php
+++ public/index.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+require_once '../backend/config/database.php';
 
-<body>
-
-</body>
-
-</html>
+echo "Conexão OK!";
